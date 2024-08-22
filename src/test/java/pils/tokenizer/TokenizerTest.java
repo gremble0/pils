d696ce7a451b9c3c1712c19920bdf78eb8654db2
gremble0 <hermanstornes@gmail.com>
2024-08-22 23:04:41 +0200

    Add test for tokenizing basic source file

--- src/test/java/pils/tokenizer/TokenizerTest.java
+++ src/test/java/pils/tokenizer/TokenizerTest.java
@@ -4,11 +4,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class TokenizerTest {
   @Test
   public void testConstructor() {
-    Assertions.assertThrows(FileNotFoundException.class, () -> { new Tokenizer(""); });
-    Assertions.assertDoesNotThrow(() -> { new Tokenizer("src/test/source/empty.pls"); });
+    Assertions.assertThrows(FileNotFoundException.class, () -> new Tokenizer(""));
+    Assertions.assertDoesNotThrow(() -> new Tokenizer("src/test/tokenizer/source/empty.pls"));
+  }
+
+  @Test
+  public void testBasicTokenizer() throws FileNotFoundException {
+    Tokenizer tokenizer = new Tokenizer("src/test/tokenizer/source/basic.pls");
+    PrintStream logStream = new PrintStream(new FileOutputStream("src/test/tokenizer/logs/basic.log"));
+    PrintStream expectedStream = new PrintStream(new FileOutputStream("src/test/tokenizer/expected/basic.log"));
+    tokenizer.setLogStream(logStream);
+
+    while (!tokenizer.eof())
+      tokenizer.advance();
+
+    Assertions.assertEquals(logStream, expectedStream);
   }
 }
